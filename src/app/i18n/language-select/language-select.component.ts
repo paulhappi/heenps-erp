7572cf6c108905a31e3b81ed026c0d73b5a3939d
Happi Yamgoue <phappi@heenps.org>
2022-07-21 00:58:12 +0200

    i18n corrected
    i18n corrected

--- src/app/i18n/language-select/language-select.component.ts
+++ src/app/i18n/language-select/language-select.component.ts
@@ -12,16 +12,13 @@ export class LanguageSelectComponent implements OnInit {
   TransLang=[];
 
   constructor(public translate: TranslateService) {
-    //translate.setDefaultLang('english');
     translate.addLangs(['english', 'français']);
-    //translate.use('english');
 
-    if (localStorage.getItem('locale')) {
-      const browserLang = localStorage.getItem('locale');
-      translate.use(browserLang.match(/english|français/) ? browserLang : 'english');
-     } else {
-     localStorage.setItem('locale', 'english');
-     translate.setDefaultLang('english');
+    if (localStorage.getItem('language')) {
+      translate.use(localStorage["language"]);
+    } else {
+      localStorage.setItem('language', 'english');
+      translate.use('english');
     }
   }
 
